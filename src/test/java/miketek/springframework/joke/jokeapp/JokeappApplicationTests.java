7ac9985ec148564f94d049d04c78adadfcc8329b
Miguel Suarez <miguel.suarez@nearshoretechnology.com>
2020-08-26 19:16:36 -0500

    first commit

---
+++ src/test/java/miketek/springframework/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package miketek.springframework.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
